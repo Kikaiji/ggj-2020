280ac9d58c2329f15aac28fbfa0e2bde72cb8d86
Kikaiji <60505347+Kikaiji@users.noreply.github.com>
2020-02-02 16:42:15 +1030

    4:42

--- epic gaming jam/Assets/Scripts/Game/ResourceManager.cs
+++ epic gaming jam/Assets/Scripts/Game/ResourceManager.cs
@@ -8,10 +8,13 @@ public class ResourceManager : MonoBehaviour
 {
     [SerializeField]
     GameObject resourcebox;
+    float sum;
+    TownManager tmanager;
     public float[] resources = new float[5];
     // Start is called before the first frame update
     void Start()
     {
+        tmanager = GameObject.Find("TownManager").GetComponent<TownManager>();
         resourcebox = GameObject.Find("ResourceBox");
         DontDestroyOnLoad(gameObject);
         for(int i = 0; i < 3; i++)
@@ -23,6 +26,8 @@ public class ResourceManager : MonoBehaviour
         {
             resourcebox.transform.GetChild(i).GetComponent<Text>().text = resources[i].ToString();
         }
+        //for(int i = 0; i < )
+        resourcebox.transform.GetChild(resources.Length).GetComponent<Text>().text = "5"; 
     }
 
     // Update is called once per frame
