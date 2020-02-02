41e3bc5ad7725ad71c45fbc7cade2a1350a5afcd
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2020-02-02 14:36:19 +1030

    Add cinematic

---
+++ epic gaming jam/Assets/Scripts/Cinematic/CinematicController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CinematicController : MonoBehaviour
+{
+
+    public GameObject title;
+    private float titleOpacity = 1;
+
+    public GameObject withoutTent;
+    private float panY = 0;
+
+
+    public GameObject withTent;
+
+    public GameObject nomad;
+    
+    void Start()
+    {
+        StartCoroutine(HideTitle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator HideTitle()
+    {
+        yield return new WaitForSeconds(1);
+        titleOpacity = 0;
+        title.SetActive(false);
+
+        withoutTent.SetActive(true);
+        StartCoroutine(PanUp());
+    }
+
+    IEnumerator PanUp()
+    {
+        while (panY < 11) {
+            yield return new WaitForEndOfFrame();
+            panY += 0.05f;
+            transform.position = new Vector3(transform.position.x, panY, transform.position.z);
+        }
+        yield return new WaitForSeconds(3);
+        withoutTent.SetActive(false);
+        withTent.SetActive(true);
+        nomad.SetActive(true);
+        StartCoroutine(PanDown());
+    }
+
+    IEnumerator PanDown()
+    {
+        while (panY > 0)
+        {
+            yield return new WaitForEndOfFrame();
+            panY -= 0.05f;
+            transform.position = new Vector3(transform.position.x, panY, transform.position.z);
+        }
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadSceneAsync("TownScene");
+    }
+}
